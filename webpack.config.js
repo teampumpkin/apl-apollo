32730ac839a3bdf598358c1ae9b1d30347d243c4
MedVik <vikrant.agrawal@medlife.com>
2019-12-22 19:05:58 +0530

    css changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const outputDirectory = 'dist';
-var ImageminPlugin = require('imagemin-webpack-plugin').default
-const glob = require('glob');
 
 module.exports = {
   entry: ['babel-polyfill', './src/client/index.js'],
@@ -43,14 +41,6 @@ module.exports = {
     }
   },
   plugins: [
-    new ImageminPlugin({
-      externalImages: {
-        context: 'src/client', // Important! This tells the plugin where to "base" the paths at
-        sources: glob.sync('src/client/assets/**/*.png'),
-        destination: 'src/public/assets',
-        fileName: '[path][name].[ext]' // (filePath) => filePath.replace('jpg', 'webp') is also possible
-      }
-    }),
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
       template: './public/index.html',
